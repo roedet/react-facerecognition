1939d0132234917d1ed40b71143497ac7cbf89f7
roedet <roedet@users.noreply.github.com>
2023-01-27 22:28:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-facerecognition
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-uw5jfk)
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+import Login from './components/Login/Login';
+
+
+export default function App() {
+  return (
+    <div>
+      <Login/>
+      <h1>Hello StackBlitz!</h1>
+      <h2>hello too</h2>
+      <p>Start editing to see some magic happen ok :)</p>
+    </div>
+  );
+}
+
