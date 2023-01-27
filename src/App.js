39d5f60b503b25c739389755b57d4b2a080903ce
roedet <roedet@users.noreply.github.com>
2023-01-27 23:11:23 +0100

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './style.css';
 import Login from './components/Login/Login';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 
 export default function App() {
   return (
     <div>
       <Login />
       <Logo />
+      <ImageLinkForm />
+      <Rank />
       <h1>Hello StackBlitz!</h1>
-      <h2>hello too</h2>
-      <p>Start editing to see some magic happen ok :)</p>
     </div>
   );
 }
---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const imagelinkForm = () => {
+  return (
+    <div>
+      <p>Give link</p>
+      <div>
+        <input type="text" />
+        <button>press</button>
+      </div>
+    </div>
+  );
+};
+
+export default imagelinkForm;
