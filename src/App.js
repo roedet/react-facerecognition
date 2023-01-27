125ad4373e910e0e0756540abb11819a2bd0af8e
roedet <roedet@users.noreply.github.com>
2023-01-27 22:34:40 +0100

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 import Login from './components/Login/Login';
-
+import Logo from './components/Logo/Logo';
 
 export default function App() {
   return (
     <div>
-      <Login/>
+      <Login />
+      <Logo />
       <h1>Hello StackBlitz!</h1>
       <h2>hello too</h2>
       <p>Start editing to see some magic happen ok :)</p>
     </div>
   );
 }
-
